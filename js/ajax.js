b5e4a9262043aa6ac2a1fde740e9fa1e00202e17
Your Name <you@example.com>
2019-10-30 11:06:32 +0800

    第三次封装功能，增加处理用户传递不同数据类型处理功能

--- js/ajax.js
+++ js/ajax.js
@@ -23,9 +23,20 @@ function ajax(options) {
     xhr.open(options.type, options.url);
     // 如果是post请求
     if (options.type == 'POST') {
+        // 用户希望向服务器端请求的参数的数据类型
+        var contnetType = options.herder['Content - Type'];
         // 配置请求参数格式类型
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form/urlencoded');
-        xhr.send(params)
+        xhr.setRequestHeader('Content-Type', contnetType);
+        // 判断用户希望传递的数据类型
+        // 如果为json
+        if (contnetType === 'application/json') {
+            // 向服务器端传递json数据格式的参数
+            xhr.send(JSON.stringify(options.data))
+        } else {
+            // 向服务器传递普通类型的请求参数
+            xhr.send(params)
+        }
+
     } else {
         // 直接发送请求
         xhr.send()
@@ -42,9 +53,12 @@ function ajax(options) {
 ajax({
         type: 'get', //请求方式
         url: '01.html', //请求地址
-        data: {
+        data: { //请求参数
             name: 'zhaangsahng',
             age: 18
+        },
+        herder: { //请求参数类型
+            'Content - Type': 'application/x-www-form/urlencoded'
         },
         success: function(data) { //成功则调用  data参数哪来的？
             console.log(data)
