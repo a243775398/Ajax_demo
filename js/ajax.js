da1e23879d8af64544e6583e07db0bebf2a9ef4e
Your Name <you@example.com>
2019-10-30 10:46:18 +0800

    二次封装，完成不同请求方式，不同参数格式的传递

--- js/ajax.js
+++ js/ajax.js
@@ -1,8 +1,35 @@
 function ajax(options) {
     // 创建Ajax对象
     var xhr = new XMLHttpRequest();
+    // 接受请求参数的变量
+    var params = '';
+    for (const attr in options.data) {
+        params += attr + '=' + options.data[attr] + '&';
+    }
+    /*
+    要将参数处理成这个格式
+    {name:'zhangsang',age=18}
+    name=zhangsang&age=18
+    */
+    // 截取字符串最后一个
+    params = params.substr(0, params.length - 1);
+
+    // 判断请求方式
+    if (options.type == 'GET') {
+        // 拼接参数
+        options.url = options.url + '?' + params;
+    }
     // 配置Ajax对象
     xhr.open(options.type, options.url);
+    // 如果是post请求
+    if (options.type == 'POST') {
+        // 配置请求参数格式类型
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form/urlencoded');
+        xhr.send(params)
+    } else {
+        // 直接发送请求
+        xhr.send()
+    }
     // 发送请求
     xhr.send();
     // 监听onload事件
@@ -13,13 +40,32 @@ function ajax(options) {
 }
 
 ajax({
-    type: 'get', //请求方式
-    url: '01.html', //请求地址
-    success: function(data) { //成功则调用
-        console.log(data)
-    },
-    error: function(data) {
-        console.log(data)
-    }
+        type: 'get', //请求方式
+        url: '01.html', //请求地址
+        data: {
+            name: 'zhaangsahng',
+            age: 18
+        },
+        success: function(data) { //成功则调用  data参数哪来的？
+            console.log(data)
+        },
+        error: function(data) { //失败则调用
+            console.log(data)
+        }
 
-})
+    })
+    /*
+        请求参数要考虑的问题
+            1请求参数的位置
+            将请求参数传递到ajax函数内部，在函数内部根据请求方式的不同放在不同的位置
+                post放在 send()中
+                get放在地走栏中
+
+            2.请求格式的问题
+            application/x-www-form-urlencoded
+                name=shangsang&age=18
+            application/json
+                {name:'zhangsang,age:18'}
+            传递对象数据类型对调用
+
+    */
